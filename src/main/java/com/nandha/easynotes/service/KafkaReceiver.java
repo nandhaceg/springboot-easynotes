0584fae6a84a8a99298425eda2f382ff89da44d3
Nandha Kumar <visitnandha95@gmail.com>
2020-08-02 18:34:45 +0530

    logger update

--- src/main/java/com/nandha/easynotes/service/KafkaReceiver.java
+++ src/main/java/com/nandha/easynotes/service/KafkaReceiver.java
@@ -1,5 +1,7 @@
 package com.nandha.easynotes.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,16 +12,18 @@ import com.nandha.easynotes.properties.ApplicationProperties;
 @Service
 public class KafkaReceiver {
 	
+	private static final Logger logger = LogManager.getLogger(KafkaReceiver.class);
+	
 	@Autowired
 	ApplicationProperties applicationProperties;
 	
 	@KafkaListener(topics = "${message.topic.name}", groupId = "group-id")
 	public void listen(String message) {
-		System.out.println("Received Messasge in group - group-id: " + message);
+		logger.debug("Received Messasge in group - group-id: " + message);
 	}
 	
 	@KafkaListener(topics = "${note.topic.name}", groupId = "note" , containerFactory = "noteKafkaListenerFactory")
-    public void noteListener(Note note) {
-        System.out.println("Recieved note message: " + note);
-    }
+ 	public void noteListener(Note note) {
+		logger.debug("Recieved note message: " + note);
+    	}
 }
